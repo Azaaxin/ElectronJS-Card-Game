ee91d41b965d16a1be23bd7e6c4bc9745d543d06
Azaaxin <ludvig_olausson@hotmail.com>
2020-01-30 15:19:33 +0100

    Working better now, looping out playground and clearing it

--- Reey-win32-x64/resources/app/scripts/gameboard.js
+++ Reey-win32-x64/resources/app/scripts/gameboard.js
@@ -7,7 +7,12 @@ function make_playground(){
     //Loop out cards on the playground
     var i;
     for (i = 0; i < level; i++) {
-        var div = "<div class='card'>" + i + "</div>";
+        var div = document.createElement('div');
+        div.textContent = i;
+        div.setAttribute('class', 'card');
         document.getElementById("wrapper").appendChild(div);
     }
+}
+function clear_playground(){
+    document.getElementById("wrapper").innerHTML = "";
 }
