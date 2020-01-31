8faaed104e2b9e093bc18e9d425f606097304daf
Azaaxin <ludvig_olausson@hotmail.com>
2020-01-31 22:20:18 +0100

    Added, sketch to comments

--- scripts/gameboard.js
+++ scripts/gameboard.js
@@ -16,3 +16,29 @@ function make_playground(){
 function clear_playground(){
     document.getElementById("wrapper").innerHTML = "";
 }
+
+/*
+concept:
+            <div id="1" class="card" onClick="reply_click(this.id)">1</div>
+            <div id="2" class="card" onClick="reply_click(this.id)">2</div>
+            <div id="3" class="card" onClick="reply_click(this.id)">3</div>
+                
+            <script type="text/javascript">
+            function reply_click(clicked_id)
+            {
+                alert(clicked_id);
+            }
+            </script>
+
+pseudocode:
+            if (exists) clicked_id= randomized[x] = true
+                if (value already added skip(Done with array, if exists/contains)) = false
+                    variable add 1
+                        if variable(int) == all answers(int) = true
+                            next round
+                            add score
+            else
+                health - 1
+                if health < 5
+                    gameover
+*/
